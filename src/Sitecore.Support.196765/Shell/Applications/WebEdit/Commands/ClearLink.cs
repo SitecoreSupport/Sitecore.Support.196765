9b6452d3a5c04674e5b46615fd70ab594756a252
Eugene Oparyshev <eop@sitecore.net>
2017-12-04 15:28:33 +0200

    Default realization

---
+++ src/Sitecore.Support.196765/Shell/Applications/WebEdit/Commands/ClearLink.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Support.Shell.Applications.WebEdit.Commands
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.Shell.Applications.WebEdit.Commands;
+    using Sitecore.Shell.Framework.Commands;
+    using Sitecore.Web;
+    using Sitecore.Web.UI.Sheer;
+    public class ClearLink : WebEditLinkCommand
+    {
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            ExplodeParameters(context);
+            string formValue = WebUtil.GetFormValue("scPlainValue");
+            context.Parameters.Add("fieldValue", formValue);
+            Context.ClientPage.Start(this, "Run", context.Parameters);
+            
+        }
+    }
+}
